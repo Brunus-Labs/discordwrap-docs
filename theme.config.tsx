4e1b27dcffe8013da5e70cf9017f1b4c1f6a36c4
josh <josh@mjbrowns.com>
2023-04-12 14:08:07 -0400

    fix: meta and introduction

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,22 +1,24 @@
-import {DocsThemeConfig} from 'nextra-theme-docs'
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: <span>Discordwrap</span>,
   project: {
-    link: 'https://github.com/Brunus-Labs/discordwrap-docs',
+    link: "https://github.com/Brunus-Labs/discordwrap-docs",
   },
-  docsRepositoryBase: 'https://github.com/Brunus-Labs/discordwrap-docs/tree/main',
+  docsRepositoryBase:
+    "https://github.com/Brunus-Labs/discordwrap-docs/tree/main",
   editLink: {
     text: "Edit this page",
   },
   footer: {
-    text: 'Discordwrap Docs',
+    text: "Discordwrap Docs",
   },
   useNextSeoProps() {
     return {
-      titleTemplate: "DW - %s"
-    }
-  }
-}
+      titleTemplate: "DW - %s",
+      description: "%s\nThe Discordwrap Docs",
+    };
+  },
+};
 
-export default config
+export default config;
